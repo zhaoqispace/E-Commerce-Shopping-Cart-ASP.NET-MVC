bd1ace9aef44c1829147d698a030ee2ba0072698
Aye Phyu Sin <ayephyusin18994@gmail.com>
2021-04-12 09:18:58 +0800

    Update ProductController.cs

--- Core2Base/Controllers/ProductController.cs
+++ Core2Base/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core2Base.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Core2Base.Data;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Core2Base.Controllers
