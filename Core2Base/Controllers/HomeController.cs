b2fd1d77c9f31d170baa782c6a3ceb389a4d3c78
Aye Phyu Sin <Aye Phyu Sin@DESKTOP-0UHTQ0S>
2021-04-12 15:28:16 +0100

    add: comment in homecontroller

--- Core2Base/Controllers/HomeController.cs
+++ Core2Base/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Core2Base.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         //combine product controller with home controller
+        //test commit
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
