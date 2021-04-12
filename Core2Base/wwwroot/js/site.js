dd41b33a2d2324594b5efdc25529082b407d838b
Wenquan Chen <e0696865@u.nus.edu>
2021-04-13 05:09:53 +0800

    Added in Shopping cart + - increment decrement functions. Prevents negative values from being entered into shopping cart

--- Core2Base/wwwroot/js/site.js
+++ Core2Base/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('.plusButton').click(function Add() {
+    numberInCart = $(this).prev();
+    let counter = 1;
+    numberInCart.val(parseInt(numberInCart.val(), 10) + counter);
+});
+
+$('.minusButton').click(function Minus() {
+    numberInCart = $(this).next();
+    let counter = 1;
+    if (numberInCart.val() > 0 && numberInCart.val() != 0) {
+        numberInCart.val(parseInt(numberInCart.val(), 10) - counter);
+    }
+});
