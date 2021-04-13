bfcadbc25d298556f207aa68f9187a9c0ad0248a
RonnieFun <ronyfun@hotmail.com>
2021-04-13 12:22:20 +0800

    Resolved Conflicts - Done compare login info with database

--- Core2Base/Controllers/HomeController.cs
+++ Core2Base/Controllers/HomeController.cs
@@ -66,7 +66,7 @@ namespace Core2Base.Controllers
             {
                 User user = UserData.GetUserInfo(email);
 
-                if (BC.Verify(password, user.Password))
+                if (user != null && BC.Verify(password, user.Password))
                 {
                     return RedirectToAction("Index", "Home");
                 }
