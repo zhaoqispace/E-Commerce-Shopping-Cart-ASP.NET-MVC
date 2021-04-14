0a5c4a6d845d47712e7140ddb82b5fd59ca98e81
Wenquan Chen <e0696865@u.nus.edu>
2021-04-14 22:17:36 +0800

    Coded JS to ensure minimum quantity in cart is 0 and maximum quantity is 99

--- Core2Base/wwwroot/js/site.js
+++ Core2Base/wwwroot/js/site.js
@@ -5,14 +5,33 @@
 
 $('.plusButton').click(function Add() {
     numberInCart = $(this).prev();
-    let counter = 1;
-    numberInCart.val(parseInt(numberInCart.val(), 10) + counter);
+    let counter = 1;  
+    if (numberInCart.val() < 99) {
+        numberInCart.val(parseInt(numberInCart.val(), 10) + counter);
+    }
+    if (numberInCart.val() >= 99) {
+        numberInCart.val(parseInt(numberInCart.val(), 10) + 0);
+        alert("Sorry, The purchase limit for each product is 99.");
+    }
 });
-
+        
 $('.minusButton').click(function Minus() {
     numberInCart = $(this).next();
     let counter = 1;
-    if (numberInCart.val() > 0 && numberInCart.val() != 0) {
+    if (numberInCart.val() > 1 && numberInCart.val() != 0) {
         numberInCart.val(parseInt(numberInCart.val(), 10) - counter);
+    }
+});
+
+$(".quantity").on("keyup", function () {
+
+    if ($(this).val() < 1 && $(this).val() != ' ') {
+        alert("Sorry, the minimum purchase quantity is 1. Please click the Remove All button to remove product from your cart.");
+        $(this).val(1);
+    }
+
+    if ($(this).val() > 99) {
+        alert("Sorry, The purchase limit for each product is 99.");
+        $(this).val(99);
     }
 });
