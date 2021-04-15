f30125e818d0559aa3a64a5260affceb5a76a67c
RonnieFun <ronyfun@hotmail.com>
2021-04-15 21:52:37 +0800

    Shopping Cart links to logged in UserID

--- Core2Base/Models/CartDetail.cs
+++ Core2Base/Models/CartDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,14 @@ namespace Core2Base.Models
 {
     public class CartDetail
     {
-        public string CartId;
-
-        public string ProductId;
-
-        public int qty;
+        public int CartId { get; set; }
+        public Guid ProductId { get; set; }
+        public int qty { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+        public string ProductCat { get; set; }
+        [DataType(DataType.Currency)]  // to show dollar sign
+        public double UnitPrice { get; set; }
+        public string ProductImg { get; set; }
     }
 }
